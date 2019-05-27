9901b2298cab2afe73c641776648a55118d8263d
Chinmay <chinmayshringi4@gmail.com>
2019-05-27 19:04:22 +0530

    finished

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
-router.get('/', function(req,res){
-    res.render('index');
-});
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+);
+
 module.exports = router;
